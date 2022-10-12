5194962d38e530f24d795a030ee939565b44b58a
jhonbg93 <jhon.betancourth03@gmail.com>
2022-10-12 04:03:53 +0200

    solucion ejercicio 2

---
+++ ejercicio 2/index.js
@@ -0,0 +1,14 @@
+var nombre = "Jhon Betancourth"
+//Edad
+var edad = 28;
+//Boolean
+var desarrollador = true;
+//Fecha de cumpleaños
+var miCumpelaños = new Date("december 11 1993")
+//Libro Favorito 
+
+const libroFavorito = {
+    titulo: "padre rico Padre pobre",
+    autor: "Robert Kiyosaki",
+    fecha: new Date("december 05 2019")
+}
