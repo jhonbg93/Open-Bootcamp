7d9121831795cb56373321ff48ee25ef622cfdfa
jhonbg93 <jhon.betancourth03@gmail.com>
2022-10-12 04:12:13 +0200

    Correccion ejercicios

--- ejercicio 2/index.js
+++ ejercicio 2/index.js
@@ -1,14 +1,15 @@
-var nombre = "Jhon Betancourth"
-//Edad
-var edad = 28;
-//Boolean
-var desarrollador = true;
-//Fecha de cumpleaños
-var miCumpelaños = new Date("december 11 1993")
-//Libro Favorito 
+const miInformacion = ["Jhon Betancourth", 28, true, new Date(1993, 12, 11)];
+// var nombre = "Jhon Betancourth"
+// //Edad
+// var edad = 28;
+// //Boolean
+// var desarrollador = true;
+// //Fecha de cumpleaños
+// var miCumpelaños = new Date("december 11 1993")
+// //Libro Favorito
 
 const libroFavorito = {
-    titulo: "padre rico Padre pobre",
-    autor: "Robert Kiyosaki",
-    fecha: new Date("december 05 2019")
-}
+  titulo: "padre rico Padre pobre",
+  autor: "Robert Kiyosaki",
+  fecha: new Date("december 05 2019"),
+};
