836eb09f07cc09afe119b0cf5f512df7c4a072ae
jhonbg93 <jhon.betancourth03@gmail.com>
2022-10-12 12:35:10 -0500

    soluciones con correccion

--- ejercicio 3/factorial-bucle-for.js
+++ ejercicio 3/factorial-bucle-for.js
@@ -23,3 +23,10 @@ function factorialize(num) {
 }
 factorialize(5);
 
+//Solucion open bootcamp
+
+let factorial = 1
+for (let i = 10; i > 0; i--) {
+    factorial *= i
+}
+console.log(factorial) // Factorial de 10 = 3628800
--- ejercicio 3/factorial-bucle-while.js
+++ ejercicio 3/factorial-bucle-while.js
@@ -14,3 +14,12 @@ function factorial(num) {
     console.log(result);
 }
 factorial(5);
+
+//solucion open bootcamp
+let factorial = 1
+let num = 10
+while (num > 1) {
+    factorial *= num
+    num--
+}
+console.log(factorial) // Factorial de 10 = 3628800
