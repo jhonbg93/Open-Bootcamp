2759802644c1f8df42a876234706f0eaecf3cc3e
jhonbg93 <jhon.betancourth03@gmail.com>
2022-10-24 15:21:36 +0200

    ES6

--- sesion-10-Modulos/modulos-ES6/index.js
+++ sesion-10-Modulos/modulos-ES6/index.js
@@ -1,6 +1,20 @@
 //para exportar modulos con es6 lo primere que debemos hacer es modificar el package.json
 //colocando "type" y colocamos en vez de commonJS colocamos module.
 // luego colocamos en cada una de las funciones el export al inicio de cada funcion.
-import { suma } from "./modulos/matematicas.js";
-const sum = suma(5, 10);
+// import { suma } from "./modulos/matematicas.js";
+
+// si quiero importar todas las funciones del fichero podemos hacerlo de la siguiente manera
+import * as operaciones from "./modulos/matematicas.js";
+// de esta manera podemos exportar por dafault
+import getAutor from "./modulos/literatura.js";
+
+const sum = operaciones.suma(1, 6);
 console.log(sum);
+
+const rest = operaciones.resta(5, 10);
+console.log(rest);
+
+const mult = operaciones.multiplicar(5, 10);
+console.log(mult);
+
+console.log(getAutor());
---
+++ sesion-10-Modulos/modulos-ES6/modulos/literatura.js
@@ -0,0 +1,5 @@
+const getAutor = () =>{
+    console.log("Miguel de Cervantes")
+}
+
+export default getAutor;
